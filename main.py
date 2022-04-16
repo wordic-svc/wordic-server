789f7ecb081d3106a9bb8a39f4ad47e42ee362de
loeaf <ckdic8524@naver.com>
2022-04-16 14:27:54 +0900

    신규 API 추가

--- main.py
+++ main.py
@@ -2,15 +2,26 @@ from concurrent.futures import ThreadPoolExecutor
 
 from kiwipiepy import Kiwi
 from googletrans import Translator
+
 import abbreviate
 import std_data
 from fastapi import Depends, FastAPI, APIRouter
 import asyncio
 
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, validator, field_validator
+from typing import Optional
 
 app = FastAPI()
 
+
+class Result(BaseModel):
+    kebab_case: str
+    camel_case: str
+    snake_case_l: str
+    snake_case_s: str
+    pascal_case: str
+    combined_text: list
 
 app.add_middleware(
     CORSMiddleware,
@@ -23,9 +34,11 @@ app.add_middleware(
 # APIRouter 인스턴스를 생성합니다.
 router = APIRouter()
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
 
 # /text/{name} 엔드포인트
 @router.get("/text/{name}", response_model=dict)
@@ -54,6 +67,34 @@ async def say_hello(name: str):
                 "eng_name": f"{obj}"
             })
         return {'result': arr}
+
+
+# /text/{name} 엔드포인트
+@router.get("/text2/{name}", response_model=dict)
+async def say_hello(name: str):
+    obj = split_kor2_eng(name)
+    attrive_text = list(map(lambda x: x.attrive_text, obj))
+    result = Result(
+        kebab_case='',
+        camel_case='',
+        snake_case_l='',
+        snake_case_s='',
+        pascal_case='',
+        combined_text=[]
+    )
+    result.pascal_case = list_to_pascal_case(attrive_text)
+    result.camel_case = list_to_camel_case(attrive_text)
+    result.snake_case_l = list_to_snake_case(attrive_text).upper()
+    result.snake_case_s = list_to_snake_case(attrive_text).lower()
+    result.kebab_case = list_to_kebab_case(attrive_text)
+    result.combined_text = attrive_text  # Assuming you want to add 'attrive_text' to 'combined_text'
+
+    return {'result': {
+        "info": result,
+        "words": obj
+    }}
+
+
 # 텍스트를 처리할 함수
 # /text/{name} 엔드포인트
 @router.get("/eng/{name}", response_model=dict)
@@ -62,24 +103,77 @@ async def say_hello(name: str):
         "eng_name": f"{name}",
         "attr_name": f"{eng2attrive(name)}"
     }}
+
+
 # 텍스트를 처리할 함수
 def process_text(text):
     # 여기에 텍스트 처리 로직을 추가하십시오 (kor2_eng_col 함수 등을 사용)
     result = f"{kor2_eng_col(text)}"  # 예시: kor2_eng_col 함수로 처리
     return result
 
+
 def snake_to_camel(snake_case):
     words = snake_case.split('_')  # 스네이크 케이스 문자열을 밑줄로 분할하여 단어 목록 생성
     camel_case = words[0] + ''.join(word.capitalize() for word in words[1:])  # 첫 번째 단어는 그대로 두고 나머지 단어는 첫 글자 대문자로 변경
     return camel_case
+
+
+def list_to_kebab_case(arr):
+    return '-'.join(arr)
+
+
 def list_to_snake_case(arr):
-    snake_case_str = '_'.join(str(item).upper() for item in arr)
-    return snake_case_str
+    return '_'.join(arr)
+
+
 def list_to_camel_case(arr):
-    camel_case_str = ''.join(str(item) for item in arr)
-    # 첫 번째 문자열은 그대로 두고 나머지 문자열의 첫 글자를 대문자로 변경
-    camel_case_str = camel_case_str[0].lower() + camel_case_str[1:]
-    return camel_case_str
+    return arr[0] + ''.join(map(lambda x: x[0].upper() + x[1:], arr[1:]))
+
+
+def list_to_pascal_case(arr):
+    return ''.join(map(lambda x: x[0].upper() + x[1:], arr))
+
+
+class Word(BaseModel):
+    kor_text: str
+    eng_text: str
+    attrive_text: str
+    pos: str
+    code: str
+def pos_code_to_korean(pos_code):
+    pos_mapping = {
+        'NNG': '일반 명사',
+        'NNP': '고유 명사',
+        'SL': '외국어',
+        'XSN': '명사파생 접미사',
+        'SN': '숫자',
+        # 추가적인 품사 코드에 대한 매핑을 진행하려면 여기에 추가하세요.
+    }
+
+    return pos_mapping.get(pos_code, '알 수 없는 품사')
+
+def split_kor2_eng(inputTxt):
+    kiwi = Kiwi()
+
+    trans = Translator()
+    arr = []
+    items = kiwi.tokenize(inputTxt)
+    for index, item in enumerate(items):
+        if item[1] not in ['NNG', 'NNP', 'SL', 'XSN', 'SN']:
+            continue
+        text = item[0]
+
+        result = trans.translate(text, dest='en', src='ko')
+        arr.append(Word(
+            kor_text=text,
+            eng_text=result.text.lower(),
+            attrive_text=eng2attrive(result.text.lower()),
+            pos=str(index),
+            code=pos_code_to_korean(item[1])
+        ))
+    return arr
+
+
 def kor2_eng_col(inputTxt):
     kiwi = Kiwi()
 
@@ -109,6 +203,7 @@ def kor2_eng_col(inputTxt):
     print(arr)
     return list_to_snake_case(arr)
 
+
 def eng2attrive(text):
     if std_data.word_to_chng.get(text) is not None:
         text = std_data.word_to_chng[text]
@@ -122,6 +217,7 @@ def eng2attrive(text):
 
     print(text)
     return text
+
 
 # "/wordic-api" 경로 아래에 라우터를 추가합니다.
 app.include_router(router, prefix="/wordic-api")
