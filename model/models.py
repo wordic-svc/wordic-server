615b0432038bb1fb3821b1a36842718f73a913da
loeaf <ckdic8524@naver.com>
2022-05-16 16:01:06 +0900

    공공표준 사전 모델 추가

--- model/models.py
+++ model/models.py
@@ -9,39 +9,34 @@ from sqlalchemy import DateTime
 Base = declarative_base()
 
 '''
-폰트마스터
+단어마스터
 '''
-
-
-class FnMaster(Base):
-    __tablename__ = "TL_FN_MSTR"
-    __table_args__ = {'comment': '폰트마스터'}
+class FnWord(Base):
+    __tablename__ = "TL_WORD"
+    __table_args__ = {'comment': '단어 정보'}
 
     id = Column(String, name="ID", primary_key=True, default=lambda: str(uuid.uuid4()), comment="아이디")
+    word = Column(String, name="WORD", comment="단어")
+    word_abbr = Column(String, name="WORD_ABBR", comment="단어 약어")
+    word_eng = Column(String, name="WORD_ENG", comment="단어 영문")
+    word_desc = Column(String, name="WORD_DESC", comment="단어 설명")
     reg_dt = Column("REG_DT", DateTime, default=datetime.utcnow, comment="등록일시")
     mdfcn_dt = Column("MDFCN_DT", DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, comment="수정일시")
 
-    fnInfos = relationship("FnInfo", back_populates="fn_mstr")
 
 '''
-폰트정보
+용어마스터
 '''
-class FnInfo(Base):
-    __tablename__ = "TL_FN_INFO"
-    __table_args__ = {'comment': '폰트정보'}
+class FnWording(Base):
+    __tablename__ = "TL_WORDING"
+    __table_args__ = {'comment': '용어 정보'}
 
     id = Column(String, name="ID", primary_key=True, default=lambda: str(uuid.uuid4()), comment="아이디")
-    fi_nm = Column(String, name="FILE_NM", comment="파일명")
-    srch_id = Column(String, name="SRCH_ID", comment="검색 아이디")
-    fn_nm = Column(String, name="FN_NM", comment="폰트명")
-    fn_desc = Column(String, name="FN_DESC", comment="폰트설명")
-    fn_thck = Column(String, name="FN_THCK", comment="폰트두께")
-    etc_info = Column(String, name="ETC_INFO", comment="기타정보")
-    ttf = Column(String, name="TTF", comment="TTF")
-    otf = Column(String, name="OTF", comment="OTF")
-    sppf = Column(String, name="SPPF", comment="판매가")
+    wording_abbr = Column(String, name="WORDING_ABBR", comment="용어 약어")
+    wording = Column(String, name="WORDING", comment="용어")
+    wording_desc = Column(String, name="WORDING_DESC", comment="용어 설명")
     reg_dt = Column("REG_DT", DateTime, default=datetime.utcnow, comment="등록일시")
     mdfcn_dt = Column("MDFCN_DT", DateTime, default=datetime.utcnow, onupdate=datetime.utcnow, comment="수정일시")
 
-    fn_mstr_id = Column("FN_MSTR", String, ForeignKey('TL_FN_MSTR.ID'))
-    fn_mstr = relationship("FnMaster", back_populates="fnInfos")
+    # fn_mstr_id = Column("FN_MSTR", String, ForeignKey('TL_FN_MSTR.ID'))
+    # fn_mstr = relationship("FnMaster", back_populates="fnInfos")
